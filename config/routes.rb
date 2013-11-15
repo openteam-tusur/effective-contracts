88b242cccff6beab2171c4e93f0aa3996094bd48
Koala <karmickoala904@gmail.com>
2013-11-15 15:38:52 +0700

    search field

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ EffectiveContracts::Application.routes.draw do
   resources :permissions,   :except => [:update]
   resources :users,         :only   => [:index]
   root :to => 'application#index'
+  get 'search' => 'search#index'
 end
