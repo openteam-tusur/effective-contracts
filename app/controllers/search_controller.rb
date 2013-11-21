d1f2e6617ed49fd56414be405c4bfb5d2537317a
PeoneEr <peoneerko@gmail.com>
2013-11-21 20:18:39 +0700

    фиксы

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,20 +6,21 @@ class SearchController < ApplicationController
     @factors.each do |factor|
       @users[factor.id]=User.where(id: factor.user_id).first
     end
-
-    @temp4=Hash.new
-    @temp=Lecturer.find(@results.id)
-    @temp1=@temp.connections
-    i=0
-    a=Array.new
-    @temp1.each do |t1|
-      a[i]=t1.value_id
-      i=i+1
-    end
-    temp3=Value.where(id: a)
-    factors=Factor.all
-    factors.each do |f|
-      @temp4[f.id]=temp3.where(factor_id: f.id)
+    unless @results.nil?
+      @temp4=Hash.new
+      @temp=Lecturer.where(@results.id).first
+      @temp1=@temp.connections
+      i=0
+      a=Array.new
+      @temp1.each do |t1|
+        a[i]=t1.value_id
+        i=i+1
+      end
+      temp3=Value.where(id: a)
+      factors=Factor.all
+      factors.each do |f|
+        @temp4[f.id]=temp3.where(factor_id: f.id)
+      end
     end
   end
 end
