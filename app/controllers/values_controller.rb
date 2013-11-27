e5e34200ce8bdd71eaa6331b4bc63429db9f6416
Koala <karmickoala904@gmail.com>
2013-11-27 11:55:49 +0700

    fix some bugs

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -7,22 +7,25 @@ class ValuesController < ApplicationController
   end
 
   def edit
-    f=Factor.find(params[:id])
-    v = f.values.create(name: params[:value][:name])
-    params[:value][:lecturers_attributes].each do |temp, names|
-      search = Lecturer.where(author: names[:author])
-      if search.count == 0 
-        l = Lecturer.create(author: names[:author])
-        l.connections.create(value_id: v.id)
-      else
-        search.first.connections.create(value_id: v.id)
+    unless params[:value][:lecturers_attributes].nil? and params[:value][:name].empty?
+      f = Factor.find(params[:id])
+      v = f.values.create(name: params[:value][:name])
+      params[:value][:lecturers_attributes].each do |temp, names|
+        search = Lecturer.where(author: names[:author])
+        if search.count == 0 
+          l = Lecturer.create(author: names[:author])
+          l.connections.create(value_id: v.id)
+        else
+          search.first.connections.create(value_id: v.id)
+        end
       end
+      redirect_to action: :index
+    else
+      redirect_to action: :show, :alert => t("атата")
     end
-    redirect_to action: :index
   end
 
   def show
     @v = Value.where(factor_id: params[:id]) # -> name
   end
-
 end
